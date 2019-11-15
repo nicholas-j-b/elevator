569ea9487fbfaa0d5904d589ee6080cdb4e60068
nicholas-j-b <31422271+nicholas-j-b@users.noreply.github.com>
2019-11-16 00:16:30 +0100

    5 - title (#1)

---
+++ src/components/layout/Title.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>
+    {{ msg }}
+  </h1>
+</template>
+
+<script>
+export default {
+  name: 'Title',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
