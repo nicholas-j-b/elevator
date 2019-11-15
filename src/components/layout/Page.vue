ee91d290033c4eb9394be1014fa2a66214b44f5c
nick <nicholas.j.brooking@gmail.com>
2019-11-16 00:14:38 +0100

    5 - title

---
+++ src/components/layout/Page.vue
@@ -0,0 +1,21 @@
+<template>
+    <Title
+    :msg="title"
+    ></title>
+</template>
+
+<script>
+import Title from './Title.vue'
+
+export default {
+    name: 'Page',
+    components: {
+        Title
+    },
+    data: function() {
+        return {
+            title: "Elevator App"
+        }
+    }
+}
+</script>
