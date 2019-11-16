f3030a51146894eb5b9491a1429bc1b14bbc912b
nicholas-j-b <31422271+nicholas-j-b@users.noreply.github.com>
2019-11-16 02:32:48 +0100

    nb/5/animation-field (#2)

---
+++ src/components/layout/Body.vue
@@ -0,0 +1,28 @@
+<script>
+import Building from '../../animation/building.js'
+
+export default {
+    inject: ['provider'],
+    props: {
+        size: {
+            type: Number,
+            default: 0
+        }
+    },
+    data() {
+        return {
+            building: new Building()
+        }
+    },
+    render() {
+        if(!this.provider.context) return null;
+        const ctx = this.provider.context;
+        console.log(ctx);
+        ctx.rect(this.building.w, this.building.h, this.size, this.size);
+        ctx.fillStyle = '#F00'
+        ctx.fill();
+        return null;
+    }
+    
+}
+</script>
